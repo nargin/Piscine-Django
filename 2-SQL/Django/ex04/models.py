fadeb87bf9cc5cbd6802e8377ea1a755207ae0ad
robin <mejumine@gmail.com>
2025-07-02 23:23:30 +0200

    add: ex04

---
+++ 2-SQL/Django/ex04/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=64, unique=True, null=False)
+    episode_nb = models.IntegerField(primary_key=True)
+    opening_crawl = models.TextField(null=True)
+    director = models.CharField(max_length=32, null=False)
+    producer = models.CharField(max_length=128, null=False)
+    release_date = models.DateField(null=False)
+
+    class Meta:
+        db_table = 'ex04_movies'
+
+    def __str__(self):
+        return self.title
